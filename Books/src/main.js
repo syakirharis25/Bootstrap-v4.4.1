5b6e993f1b4b8bad7d28e8ef119b7b26381bcb58
Muhammad Syakir bin Mohd Haris <syakirharis25@gmail.com>
2020-02-05 21:39:29 +0800

    adding Books/src/main.js file

--- Books/src/main.js
+++ Books/src/main.js
@@ -20,8 +20,20 @@ let nCount = function (selector) {
         $(this).animate({
             Counter: $(this).text()
         }, {
-            duration: 4000
+            duration: 4000,
+            easin: "swing",
+            step: function (value) {
+                $(this).text(Math.ceil(value));
+            }
         })
-
     });
 };
+
+let a = 0;
+$(window).scroll(function () {
+    let oTop = $(".numbers").offset().top - window.innerHeight;
+    if (a == 0 && $(window).scrollTop() >= oTop) {
+        a++;
+        nCount(".rect > h1");
+    }
+})
